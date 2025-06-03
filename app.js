4ec45a2a483c66eb01e688dbed00f424da702040
arashad202 <ahm.rashad.202@gmail.com>
2025-06-03 05:19:29 +0300

    Computer choice
    Create a function createComputerChoice that
    returns rock or paper or scissors randomly.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+
+// Create a function getComputerChoice
+function getComputerChoice() {
+
+    // generate a random number between 1 and 3
+    let choice = Math.floor(Math.random() * 3) + 1;
+
+    // return a word for each number
+    if (choice == 1) {
+        return "rock";
+    } else if (choice == 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+
+}
